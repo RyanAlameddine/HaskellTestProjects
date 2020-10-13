418272d5c6d8a1a185446e0e704f67fd42b6fbc3
Ryan Alameddine <rhalameddine@gmail.com>
2020-10-13 15:50:39 -0700

    Finished basic Soundex algorithm and Json Parser

--- src/JsonParser.hs
+++ src/JsonParser.hs
@@ -1,5 +1,9 @@
 module JsonParser where
 import qualified Data.Map as Map
+import Data.Functor ()
+import Data.Char ()
+import Data.Bifunctor ( Bifunctor(second) )
+import Control.Applicative ( Alternative(..) )
 
 --parser :: String -> a
 --parser :: String -> (String, a)
@@ -8,28 +12,76 @@ import qualified Data.Map as Map
 --parser :: String -> Either (Int, Int, String) (String, a)
 --type Parser a = String -> Maybe (String, a)
 
-newtype Parser a = Parser 
-    { runParser :: String -> Maybe (String, a) }
+-- function from input to Maybe (rest of input, value)
+newtype Parser a = Parser { runParser :: String -> Maybe (String, a) }
+
+
+-- instance Functor Parser where
+--     fmap f (Parser p) = Parser $ \input -> do
+--                     (input', x) <- p input
+--                     Just (input', f x)     
+
+             
+instance Functor Parser where
+    fmap f (Parser p) = 
+        let maybeMap maybeVal = fmap (second f) maybeVal 
+        in Parser $ maybeMap . p
+
+instance Applicative Parser where
+    pure x = Parser $ \input -> Just (input, x)
+    (Parser f) <*> (Parser a) = Parser $ \input -> do
+                            (input', func) <- f input 
+                            (input'', val) <- a input'
+                            Just (input'', func val)
+
+instance Alternative Parser where
+    empty = Parser $ const Nothing
+    (Parser first) <|> (Parser second) = Parser $ \input -> first input <|> second input
+
+
 
 -- >>> :t runParser
--- runParser :: Parser a -> String -> Maybe (String, a)
+-- runParser :: forall a. Parser a -> String -> Maybe (String, a)
 
 -- takes in a character and returns a parser which parses that character
-charParser :: Char -> Parser Char
-charParser x = Parser f
+char :: Char -> Parser Char
+char x = Parser f
     where 
         f (y:ys)
             | y == x = Just (ys, x)
         f _ = Nothing
 
 -- takes in a string and returns a parser which parses that string
---stringParser :: String -> Parser String
---stringParser x = sequenceA $ map charParser x
+string :: String -> Parser String
+string = sequenceA . map char
 
+digit :: Parser Char
+digit = anyCharOf ['0'..'9']
 
+int :: Parser Integer
+int = read <$> some digit
 
+-- spanParser :: (Char -> Bool) -> Parser String
+-- spanParser f = Parser $ \input -> 
+--                 let (token, rest) = span f input 
+--                 in Just (rest, token)
 
+-- notEmpty :: Parser [a] -> Parser [a]
+-- notEmpty (Parser p) = Parser $ \input -> do
+--                         (input', (x:xs)) <- p input
+--                         Just (input', (x:xs))
 
+choice :: [Parser a] -> Parser a
+choice = foldl1 (<|>)
+
+between :: Parser a -> Parser b -> Parser c -> Parser b
+between a b c = a *> b <* c
+
+anyCharOf :: [Char] -> Parser Char
+anyCharOf chars = choice (map char chars)
+
+separateBy :: Parser a -> Parser b -> Parser [b]
+separateBy separator value = (:) <$> value <*> many (separator *> value) <|> pure []
 
 
 -- JSON SPECIFIC REGION
@@ -44,7 +96,66 @@ data JsonValue
     deriving (Show, Eq)
 
 jsonNull :: Parser JsonValue
-jsonNull = undefined
+jsonNull = JsonNull <$ string "null"
+
+jsonBool :: Parser JsonValue
+jsonBool = (JsonBool True <$ string "true") <|> (JsonBool False <$ string "false")
+
+jsonNumber :: Parser JsonValue
+jsonNumber = JsonNumber <$> int
+
+jsonString :: Parser JsonValue
+jsonString = JsonString <$> stringLiteral
+
+jsonArray :: Parser JsonValue
+jsonArray = JsonArray <$> between sqrBracketO elements sqrBracketC
+    where
+        elements = btwnWhite $ separateBy comma jsonValue
+
+jsonObject :: Parser JsonValue
+jsonObject = JsonObject <$> between curlyBracketO mappedPairs curlyBracketC
+    where
+        pairs = (\key _ value -> (key, value)) <$> stringLiteral <*> colon <*> jsonValue
+        separatedPairs = separateBy comma pairs
+        mappedPairs = Map.fromList <$> separatedPairs
+
 
 jsonValue :: Parser JsonValue
-jsonValue = undefined
+jsonValue = jsonNull <|> jsonBool <|> jsonNumber <|> jsonString <|> jsonArray <|> jsonObject
+
+
+whitespaceChars :: Parser Char
+whitespaceChars = anyCharOf ['\t', '\n', '\r', '\f', '\v']
+
+whitespace :: Parser String
+whitespace = many whitespaceChars
+
+whitespace1 :: Parser String
+whitespace1 = some whitespaceChars
+
+--parser surrounded by any number of whitespace
+btwnWhite :: Parser a -> Parser a
+btwnWhite p = between whitespace p whitespace
+
+--parser for char surrounded by any number of whitespace
+charBtwnWhite :: Char -> Parser Char
+charBtwnWhite c = btwnWhite $ char c
+
+stringLiteral :: Parser String
+stringLiteral = between doubleQuote (many stringChar) doubleQuote
+
+stringChar :: Parser Char
+stringChar = Parser f
+    where
+        f (y:ys)
+            | y /= '"' = Just (ys, y)
+        f _ = Nothing
+
+
+doubleQuote   = char '"'
+curlyBracketO = charBtwnWhite '{'
+curlyBracketC = charBtwnWhite '}'
+sqrBracketO   = charBtwnWhite '['
+sqrBracketC   = charBtwnWhite ']'
+colon         = charBtwnWhite ':'
+comma         = charBtwnWhite ','
--- src/Soundex.hs
+++ src/Soundex.hs
@@ -1,16 +1,40 @@
-module WordSuggestor where
-
+module Soundex where
 
 --Soundex algorithm, use the searchFor method to find matches
 
-dictionary = map soundex ["hello", "heller", "there", "thorough", "though", "general", "kenobi" ]
+import Data.Char (toLower)
 
---evaluate the soundex of each word match, the pick the word of all items with the maximum value
-searchFor :: [String] -> String -> [String]
-dict `searchFor` word = allHighestValues
+dictionary :: [SdxCode]
+dictionary = ["ryan", "christine", "anna", "emily", "essam", "sofia", "paloma", "camillo" ]
+
+type SdxCode = String
+type Term = String
+
+--pick all the soundex code matches
+searchFor :: [SdxCode] -> Term -> [Term]
+searchFor d word = map fst $ filter filterFunc dict
     where
-        values = map (evaluateWord word) dict
-        highestValue = maximum (map snd values)
-        allHighestValues = map fst (filter (\(_, v) -> v == highestValue) values)
+        filterFunc x = snd x == snd (soundex lowerWord)
+        dict = map (soundex . map toLower) d
+        lowerWord = map toLower word
 
-soundex :: String -> (String, String)
+
+soundex :: Term -> (Term, SdxCode)
+soundex (x:xs) = (x:xs, reverse $ sdx False xs [x])
+
+sdx :: Bool -> Term -> SdxCode -> SdxCode 
+sdx _ [] s = (take 4 . (++ repeat '0')) . reverse $ s
+sdx b (x:xs) s
+    | letterCheck "bfpv"     x = sdxAddChar '1'
+    | letterCheck "cgjkqsxz" x = sdxAddChar '2'
+    | letterCheck "dt"       x = sdxAddChar '3'
+    | letterCheck "l"        x = sdxAddChar '4'
+    | letterCheck "mn"       x = sdxAddChar '5'
+    | letterCheck "r"        x = sdxAddChar '6'
+    | otherwise = sdx True xs s
+    where
+        sdxAddChar :: Char -> SdxCode
+        sdxAddChar c = sdx False xs (if head s == c then s else c:s)
+            
+letterCheck :: [Char] -> Char -> Bool
+letterCheck letters = (`elem` letters)
